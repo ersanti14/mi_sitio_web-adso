c0df437a46cd6f648a429dac50f15e07d48e275a
ERsanti14 <erazoser14@gmail.com>
2022-11-22 09:48:06 -0500

    Cambios

--- js/media-query.js
+++ js/media-query.js
@@ -1,16 +1,15 @@
 let barMenu = document.getElementById("barMenu");
-let main_Menu = document.getElementById("main_Menu") 
+let main_Menu = document.getElementById("mainMenu"); 
 
-main_Menu.addEventListener("click", function() {
+barMenu.addEventListener("click", function() {
     if (main_Menu.classList.contains("main-menu")){
-        main_Menu.classList.add("menu-1");
+        main_Menu.classList.add("main-menu-visible");
         main_Menu.classList.remove("main-menu");
     }
         else{
-            main_Menu.classList.remove("menu-1");
+            main_Menu.classList.remove("main-menu-visible");
             main_Menu.classList.add("main-menu");
         }
         
 
-    }
-});
+    });
