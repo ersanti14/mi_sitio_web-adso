7224d1a9851e7aeb666bad1f1cb9f00fae63356a
ersanti14 <erazoser14@gmail.com>
2023-03-30 07:14:10 -0500

    probando2

---
+++ js/carrito.js
@@ -0,0 +1,6 @@
+let imgPrincipal = document.getElementById("imgPrincipal");
+let mainBox = document.getElementById("mainBox")
+
+imgPrincipal.addEventListener("click",function(){
+    mainBox.classList.add("fondo-oscuro");
+});
