75a589c08e9b30fc87dd33c578692add858e6b5f
NICOLASAREIZA <nicolasareiza93@gmail.com>
2023-06-15 10:58:45 -0500

    .

---
+++ ej-javascript/js/ej-javascript.js
@@ -0,0 +1,27 @@
+//activando modod strict
+"use strict"
+
+//hola mundooo
+alert("hoklaaaaa");
+let nombreAprendiz = "santiago";
+alert(nombreAprendiz);
+
+//je 3
+
+let nombresAprendiz = "santiafo";
+let apellidosAprendiz = ""
+
+
+//eje 4
+
+let num1 = 5;
+let num2 = "3";
+
+let suma = num1 + num2;
+
+console.log(suma);
+
+///
+let num3Int = parseInt(numStr);
+
+let resultado = num
