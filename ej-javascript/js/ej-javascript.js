f29870fd49bc1eac2880249ee77b134d9a34abfd
NICOLASAREIZA <nicolasareiza93@gmail.com>
2023-06-22 11:46:05 -0500

    sss

--- ej-javascript/js/ej-javascript.js
+++ ej-javascript/js/ej-javascript.js
@@ -24,4 +24,25 @@ console.log(suma);
 ///
 let num3Int = parseInt(numStr);
 
-let resultado = num
+let resultado = num3Int
+
+
+console.log("operadores aricmeticos");
+
+let numArit = 6;
+let numArit2 = 8;
+
+cosole.log("SUMA NUMaRI + NUuRIT2" + numArit + numArit2);
+
+console.log("resta");
+
+console.log("numArit - numAri" ( numArit2 - numArit)); 
+ 
+//redondear numeros
+let reslDiv = numArit / numArit2;
+//redondear segun primedioa
+console.log(Math.round(reslDiv));
+//redondear hacia abajo
+console.log(Math.floor(reslDiv));
+//redondear hacia arriba
+console.log(Math.ceil(reslDiv));
---
+++ ej-javascript/js/jacaCadeans.js
@@ -0,0 +1,21 @@
+console.log("hola mundo desde node");
+
+let ciudad = "pitALIito";
+
+let ciudadUpper = ciudad.toUpperCase();
+
+console.log(ciudadUpper)
+
+let ciudadLow = ciudad.toLowerCase();
+
+console.log(ciudadLow);
+
+let ciudadOracion = ciudad[0].toLowerCase() + ciudad.slice(1).toLowerCase();
+console.log(ciudadOracion);
+
+
+let nombres = "saniago erazo rivera";
+
+let cantNombres  = nombres.length;
+
+console.log("longitud nombre: " + cantNombres);
