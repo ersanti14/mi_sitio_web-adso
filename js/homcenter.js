46e5a219cfd2a12e5eaee88cfaf783cea99f833b
ersanti14 <erazoser14@gmail.com>
2023-08-31 00:04:41 -0500

    annn

--- js/homcenter.js
+++ js/homcenter.js
@@ -21,48 +21,36 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 
-    
 
-    let contPrincipal = document.getElementById("containerTodo");
-    let idAnaContainer = document.getElementById("idAnaContainer");
     let idcampoElemets = document.getElementById("idcampoElemets");
     let iconClose = document.getElementById("iconClose");
 
     clickAna.addEventListener("click", function () {
-
-        
         if (idcampoElemets.classList.contains("campoElemets")) {
             idcampoElemets.classList.remove("campoElemets");
             idcampoElemets.classList.add("campoAna");
-            contPrincipal.classList.remove("containerPricipal")
-            contPrincipal.classList.add("containerPricipalClose")
         } else {
             idcampoElemets.classList.add("campoElemets");
             idcampoElemets.classList.remove("campoAna");
-            contPrincipal.classList.remove("containerPricipalCLose")
-            contPrincipal.classList.add("containerPricipal")
         }
         iconClose.classList.remove("close");
         iconClose.classList.add("closeAna");
         idAnaContainer.classList.add("ocuAna");
-
     })
     iconClose.addEventListener("click", function () {
         if (idcampoElemets.classList.contains("campoElemets")) {
             idcampoElemets.classList.remove("campoElemets");
             idcampoElemets.classList.add("campoAna");
-            contPrincipal.classList.add("containerPricipal")
-            contPrincipal.classList.remove("containerPricipalCLose")
+
         } else {
             idcampoElemets.classList.add("campoElemets");
             idcampoElemets.classList.remove("campoAna");
-            contPrincipal.classList.add("containerPricipalCLose")
-            contPrincipal.classList.remove("containerPricipal")
+
         }
         iconClose.classList.add("close");
         iconClose.classList.remove("closeAna");
         idAnaContainer.classList.remove("ocuAna");
-    }) 
+    })
 
 
 });
