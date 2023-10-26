b1bb80ce53bafa5cf737e1302b8691ed5da338ba
ersanti14 <erazoser14@gmail.com>
2023-10-25 22:51:54 -0500

    como q si

--- js/jsjuego.js
+++ js/jsjuego.js
@@ -1,18 +1,3 @@
-let tijera = document.getElementById("tijera")
-let piedra = document.getElementById("piedra")
-let papel = document.getElementById("papel")
-
-tijera.addEventListener("click",()=>{
-    alert("tijera")
-})
-piedra.addEventListener("click",()=>{
-    alert("piedra")
-})
-papel.addEventListener("click",()=>{
-    alert("papel")
-})
-
-
 
 
 document.querySelector('.rules-btn').addEventListener('click', (e) =>{
