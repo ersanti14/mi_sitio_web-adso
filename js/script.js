80e9cfd1c67938c8203df506f2b4a140aa1f219f
ersanti14 <erazoser14@gmail.com>
2023-10-25 22:46:32 -0500

    pereza

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let tijera = document.getElementById("tijera")
+let piedra = document.getElementById("piedra")
+let papel = document.getElementById("papel")
+
+tijera.addEventListener("click",()=>{
+    alert("tijera")
+})
+piedra.addEventListener("click",()=>{
+    alert("piedra")
+})
+papel.addEventListener("click",()=>{
+    alert("papel")
+})
