567ddf9202128cffc8b9e8dba78b8770c82134eb
ersanti14 <erazoser14@gmail.com>
2023-11-09 08:48:33 -0500

    juego

--- js/nubeVoladora.js
+++ js/nubeVoladora.js
@@ -1,29 +1,26 @@
-let slider = document.querySelector(".slider"); 
+let slider = document.querySelector(".slider");
 let moverDer = document.querySelector(".slider-der");
 let imagenes = document.querySelectorAll(".img-spiderman").length;
 let volarGoku = document.getElementById("pasar")
 
 let contador = 0;
 
-function moverDerecha()
-{
+function moverDerecha() {
     contador++;
 
-        if(contador > imagenes -1){
-            contador=0;
-        }
-        slider.style.transition = "all 0.5s ease";
+    if (contador > imagenes - 1) {
+        contador = 0;
+    }
+    slider.style.transition = "all 0.5s ease";
     slider.style.marginLeft = `-${contador * 100}%`;
 }
 
+
 moverDer.addEventListener("click", moverDerecha);
 
-window.addEventListener("keydown", function(event) {
-    if(event.key === "space"){
-        moverDerecha
-    }
-    
-})
+
+
+
 
 function activarVuelo() {
     nubeVoladora.classList.toggle('vuelo-activo');
@@ -33,22 +30,53 @@ volar.addEventListener("click", activarVuelo);
 
 const nubeVoladora = document.querySelector('.nubeVoladora');
 let topValue = 250;
-let leftValue = 25; 
+let leftValue = 25;
 const maxLeftValue = 1600;
 const maxTopValue = 600;
 
-window.addEventListener("keydown", function(event) {
+
+let goku = document.querySelector(".goku")
+
+
+
+
+
+
+window.addEventListener("keydown", function (event) {
     if (event.key === "ArrowRight" && leftValue + 10 <= maxLeftValue) {
-        leftValue += 150;
+        leftValue += 100;
         nubeVoladora.style.left = leftValue + "px";
+        nubeVoladora.style.transform = "scaleX(1)"
+        console.log(leftValue)
+        if (leftValue > 1550) {
+            contador++;
+
+            if (contador > imagenes - 1) {
+                contador = 0;
+            }
+            slider.style.transition = "all 5s ease";
+            slider.style.marginLeft = `-${contador * 100}%`;
+        }
+
     } else if (event.key === "ArrowLeft" && leftValue - 10 >= 0) {
-        leftValue -= 150;
+        leftValue -= 100;
+        nubeVoladora.style.transform = "scaleX(-1)"
         nubeVoladora.style.left = leftValue + "px";
+        console.log(leftValue)
+        if (leftValue < 0) {
+            contador++;
+
+            if (contador > imagenes - 1) {
+                contador = 0;
+            }
+            slider.style.transition = "all 3s ease";
+            slider.style.marginLeft = `-${contador * 100}%`;
+        }
     } else if (event.key === "ArrowDown" && topValue + 10 <= maxTopValue) {
-        topValue += 150;
+        topValue += 100;
         nubeVoladora.style.top = topValue + "px";
     } else if (event.key === "ArrowUp" && topValue - 10 >= 0) {
-        topValue -= 150;
+        topValue -= 100;
         nubeVoladora.style.top = topValue + "px";
     }
 });
