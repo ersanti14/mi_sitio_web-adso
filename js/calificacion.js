ae332c4fbcc2e7a69a49bdb3011779499013e1d8
NICOLASAREIZA <nicolasareiza93@gmail.com>
2023-11-30 08:18:29 -0500

    hola

--- js/calificacion.js
+++ js/calificacion.js
@@ -5,28 +5,41 @@ valorStar.addEventListener("submit", function (e) {
 
   var numberStar = document.getElementById("starValue").value;
   let boxStar = document.getElementById("boxStar");
+  let mensajeError = document.getElementById("mensajeError");
 
-  
   boxStar.innerHTML = "";
+  mensajeError.innerHTML = ""; // Limpiar mensajes de error previos
 
- 
-  numberStar = parseInt(numberStar);
+  numberStar = parseFloat(numberStar);
 
+  if (!isNaN(numberStar) && numberStar >= 0 && numberStar <= 5) {
+    // Redondea hacia arriba si el decimal es mayor o igual a 0.7
+    let roundedStar = Math.ceil(numberStar);
 
-  if (numberStar >= 1 && numberStar <= 5) {
-    for (let i = 0; i < numberStar; i++) {
+    // Calcula las estrellas llenas
+    let fullStars = Math.floor(roundedStar);
 
+    // Agrega las estrellas llenas
+    for (let i = 0; i < fullStars; i++) {
       let calificacionStarLlena = document.createElement("i");
-      
       calificacionStarLlena.setAttribute("class", "ti ti-star-filled");
       boxStar.appendChild(calificacionStarLlena);
     }
 
-    for (let i = numberStar; i < 5; i++) {
+    // Agrega la estrella a la mitad si es necesario
+    if (roundedStar % 1 !== 0) {
+      let calificacionStarMedia = document.createElement("i");
+      calificacionStarMedia.setAttribute("class", "ti ti-star-half-filled");
+      boxStar.appendChild(calificacionStarMedia);
+    }
+
+    // Completa con estrellas vacías
+    for (let i = Math.ceil(roundedStar); i < 5; i++) {
       let calificacionStarVacia = document.createElement("i");
       calificacionStarVacia.setAttribute("class", "ti ti-star");
       boxStar.appendChild(calificacionStarVacia);
     }
+  } else {
+    mensajeError.innerHTML = "La calificación debe ser un número entre 0 y 5.";
   }
 });
-
